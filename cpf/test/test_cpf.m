c698222e5bf32be7654e0ff5692c4128caf3c1c0
Anton <antonlodder@gmail.com>
2014-05-14 16:12:04 -0400

    fixed up pretty printing and changed adjustments

--- cpf/test/test_cpf.m
+++ cpf/test/test_cpf.m
@@ -1,13 +1,13 @@
-% close all; clear all; clc
-
-base = loadcase('case30_mod');
-faults = defineFaults(base);
-mFault = faults{47};
-myCase = mFault.applyto(base);
-
-
-myCase = myCase{1};
+function test_cpf()% close all; clear all; clc
+    base = loadcase('case30_mod');
+    faults = defineFaults(base);
+    mFault = faults{2};
+    myCase = mFault.applyto(base);
 
 
+    myCase = myCase{1};
 
-out = cpf(myCase,-1,5,0.025,true)
+
+
+    out = cpf(myCase,-1,5,0.025,true)
+end
